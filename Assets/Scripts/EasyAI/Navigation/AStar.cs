df7685d224ca2af9bff26fdb4b0f29ce45fff45b
EmperorOfBananas <acerenn01@gmail.com>
2023-03-30 23:46:23 -0400

    a4
    assignment 4

--- Assets/Scripts/EasyAI/Navigation/AStar.cs
+++ Assets/Scripts/EasyAI/Navigation/AStar.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using EasyAI.Navigation.Nodes;
+using System.Linq;
+using System;
 using UnityEngine;
 
 namespace EasyAI.Navigation
@@ -19,7 +21,58 @@ namespace EasyAI.Navigation
         public static List<Vector3> Perform(Vector3 current, Vector3 goal, List<Connection> connections)
         {
             // TODO - Assignment 4 - Implement A* pathfinding.
-            return new();
+            List<Vector3> closed = new List<Vector3>();//store the path that has been taken
+            List<AStarNode> open = new List<AStarNode>();//opened nodes
+            List<AStarNode> suc = new List<AStarNode>();//successor nodes
+            AStarNode select = new AStarNode(current, goal);//current node
+            suc = getSuccessors(select, goal, connections);
+            open = open.Concat(suc).ToList();//merge open and successors lists
+            select.Close();
+            closed.Add(select.Position);//add vector position to closed 
+            
+            //code below not working path gen takes forever
+            /*while(select.Position != goal){//if the select node != goal
+                select = visit(suc);
+                //Errors occurring in the code below
+                //path generation takes forever
+                if(suc.Count() > 0){//if there are successors
+                    select = visit(suc);
+                }
+                else{//if there are no successors
+                    select = visit(open);  
+                }
+                select.Close();
+                closed.Add(select.Position);
+                suc = getSuccessors(select, goal, connections);
+                open = open.Concat(suc).ToList();
+            }*/
+            return closed;
+        }
+    
+        //this function takes an AStarNode, goal vector, and the Connections list, and outputs a 
+        //list of AStarNode's that are connected to node
+        public static List<AStarNode> getSuccessors(AStarNode node, Vector3 goal, List<Connection> c){
+            List<AStarNode> results = new List<AStarNode>();
+            foreach (Connection conn in c){
+                if(conn.A.Equals(node.Position)){
+                    results.Add(new AStarNode(conn.B, goal, node));
+                }
+            }
+            return results;
+        }
+
+        //this function resurns a node with the lowest H cost, if there is a tie, it is decided
+        //by FIFO
+        public static AStarNode visit(List<AStarNode> suc){
+            AStarNode selection = suc[0];
+            foreach (AStarNode s in suc){
+                if(s != suc[0]){
+                    if(s.CostH <= selection.CostH){
+                        selection = s;
+                    }
+                }
+            }
+            return selection;
         }
     }
 }
--- Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
+++ Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 namespace EasyAI.Navigation.Generators
 {
@@ -11,6 +12,8 @@ namespace EasyAI.Navigation.Generators
         [Min(0)]
         [Tooltip("How far away from corners should the nodes be placed.")]
         private int cornerNodeSteps = 3;
+        public int x;//Length of NodeArea
+        public int z;//Width of NodeArea
     
         /// <summary>
         /// Place nodes at convex corners.
@@ -18,6 +21,66 @@ namespace EasyAI.Navigation.Generators
         public override void Generate()
         {
             // TODO - Assignment 4 - Complete corner-graph node generation.
+            x = NodeArea.RangeX;
+            z = NodeArea.RangeZ;
+            for(int i = 0; i < x; i++){
+                for(int j = 0; j < z; j++){
+                    if(j > 0 && j < z - 1){//if within the boundaries of RangeZ
+                        if(NodeArea.IsOpen(i, j) && !NodeArea.IsOpen(i, j-1)){//current area is open and last one was closed
+                            if(isConvex(i, j-1)){//check if last area was a convex corner
+                                Place(i, j - 1);//calculate where the node for this corner should be
+                            }
+                        }
+                        if(!NodeArea.IsOpen(i, j) && NodeArea.IsOpen(i, j-1)){//current area is closed and last one was open
+                            if(isConvex(i, j)){//check if current area is a convex corner
+                                Place(i, j);//calculate where the node for this corner should be
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //Takes the position of a potential convex corner and return true if it is so or false if otherwise
+        public bool isConvex(int cur_x, int cur_z){
+            int convex_check = 0;
+            //if there is a direction on the vertical axis that is open, 
+            //it will mean that this area has two open areas adjacent to each other.
+            //this would make it a convex corner
+            if(0 < cur_x){
+                if(NodeArea.IsOpen(cur_x - 1, cur_z)){convex_check++;}
+            }     
+            if(cur_x < x - 1){
+                if(NodeArea.IsOpen(cur_x + 1, cur_z)){convex_check++;}
+            }
+            
+            if(convex_check > 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        //Takes the position of a corner and calculates a new position to place the node for that corner
+        public void Place(int corner_x, int corner_z){
+            int node_x, node_z;
+            //if a direction leading away from the corner is clear, node_x will go 4 positions towards that direction
+            if(NodeArea.IsOpen(corner_x - 1, corner_z)){
+                node_x = corner_x - 4;
+            }
+            else{
+                node_x = corner_x + 4;
+            }
+            //same as above
+            if(NodeArea.IsOpen(corner_x, corner_z - 1)){
+                node_z = corner_z - 4;
+            }
+            else{
+                node_z = corner_z + 4;
+            }
+            //add node at position (node_x, node_z)
+            NodeArea.AddNode(node_x, node_z);
         }
     }
 }
