2b1b5354d6abc60cf45ed2bf1c048efdc6790139
EmperorOfBananas <acerenn01@gmail.com>
2023-04-20 10:03:06 -0400

    debugging capture

--- Assets/Scripts/Project/States/Capture.cs
+++ Assets/Scripts/Project/States/Capture.cs
@@ -15,17 +15,18 @@ public class Capture : State
     }
     public override void Execute(Agent agent){
         if (!s.CarryingFlag) {
-        agent.Navigate(s.EnemyFlagPosition);
-    }
-    else {
-        Debug.Log("Carrying flag, setting Retrieve state");
-        agent.SetState<Retrieve>();
-    }
+            Debug.Log("Getting flag");
+            agent.Navigate(s.EnemyFlagPosition);
+        }
+        else {
+            Debug.Log("Carrying flag, setting Retrieve state");
+            agent.SetState<Retrieve>();
+        }
 
-    if (!s.Alive) {
-        Debug.Log("Agent is dead, setting Idle state");
-        agent.SetState<Idle>();
-    }
+        if (!s.Alive) {
+            Debug.Log("Agent is dead, setting Idle state");
+            agent.SetState<Idle>();
+        }
     }
     public override void Exit(Agent agent){Debug.Log("Capture Exited");}
 }
