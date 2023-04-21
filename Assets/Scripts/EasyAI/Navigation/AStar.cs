c16e86fe6b2592ec72e8e1447d37cb20cccf00a5
EmperorOfBananas <acerenn01@gmail.com>
2023-04-21 17:56:21 -0400

    comments

--- Assets/Scripts/EasyAI/Navigation/AStar.cs
+++ Assets/Scripts/EasyAI/Navigation/AStar.cs
@@ -21,30 +21,33 @@ namespace EasyAI.Navigation
         public static List<Vector3> Perform(Vector3 current, Vector3 goal, List<Connection> connections)
         {
             // TODO - Assignment 4 - Implement A* pathfinding.
+
+            //I don't believe there is any problem with my current implementation of A*, the math seems to work, the only thing that breaks it is when goal nodes come with no connections,
+            //meaning no valid path can be found. But A* is supposed to be a complete algorithm so this cannot be the case. I feel the error must lie in the CornerGRaphGenerator.
             
-            AStarNode start = new AStarNode(current, goal);
-            List<AStarNode> open = new List<AStarNode>();
-            List<AStarNode> closed = new List<AStarNode>();
-            open.Add(start);
-            AStarNode curr = start;
-            List<Vector3> path = new List<Vector3>();
+            AStarNode start = new AStarNode(current, goal);//initialize start of algorithm
+            List<AStarNode> open = new List<AStarNode>();//initialize open list of nodes
+            List<AStarNode> closed = new List<AStarNode>();//initialize closed list of nodes
+            open.Add(start);// add start to open
+            AStarNode curr = start;// make start current node
+            List<Vector3> path = new List<Vector3>();//initialize path to return for lookup calculation
             /**
-            *forloop bellow checks if the goal node has neighbours and printes the result.
+            *forloop below checks if the goal node has neighbours and printes the result.
             **/
-            bool test = false;
+            bool test = false;//assume all goal nodes have connections
             foreach (Connection conn in connections){
-                if(conn.A.Equals(goal)){
+                if(conn.A.Equals(goal)){//if a connection in connections has goal as one of it's members, it has at least 1 neighbour and is therefore traversable
                     test = true;
                 }
             }
-            Debug.Log("goal: " + goal + " neighbours: " + test);
+            Debug.Log("goal: " + goal + " neighbours: " + test);//debug which nodes have connections and which don't
             //A* algorithm loop
             while(open.Count > 0){
-                curr = visit(open);
-                open.Remove(curr);
-                curr.Close();
-                closed.Add(curr);
-                if(curr.Position == goal){
+                curr = visit(open);//visit the most appropriate node in the open list
+                open.Remove(curr);//remove it from open list
+                curr.Close();//close it
+                closed.Add(curr);//add to closed list
+                if(curr.Position == goal){//if goal has been found, break loop
                     break;
                 }
                 //for each connection where one of the nodes is the current one and the other is not already in the open or closed lists. Add a new AStarNode
@@ -55,23 +58,23 @@ namespace EasyAI.Navigation
                 }
             }
             //get the path
-            while(curr.Position != start.Position){
-                path.Add(curr.Position);
+            while(curr.Position != start.Position){//if current position is not the start position
+                path.Add(curr.Position);//add previous positions to path so we can trace a path from the goal back to the start using the Previous AStarNode property
                 curr = curr.Previous;
             }
-            if(curr.Position == start.Position){
+            if(curr.Position == start.Position){//If we have reached the start node, we have completed the path
                 path.Add(curr.Position);
             }
-            return path;
+            return path;//return path to goal from start
         }
 
         //this function resurns a node with the lowest H cost, if there is a tie, it is decided
         //by FIFO
         public static AStarNode visit(List<AStarNode> open){
-            AStarNode select = open.ElementAt(0);
-            foreach (AStarNode o in open){
-                if(o.IsOpen){
-                    if(o.CostH <= select.CostH || !select.IsOpen){
+            AStarNode select = open.ElementAt(0);//select first element in open
+            foreach (AStarNode o in open){//each node in open is judged if it has an equal or lower CostH than 
+                if(o.IsOpen){//if current node is not closed, has not been selected as curr in A* algo yet
+                    if(o.CostH <= select.CostH || !select.IsOpen){//if select is closed, cannot be returned as new value for curr or if o is <= to CostH of select, select is equal to o
                         select = o;
                     }
                 }
--- Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
+++ Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
@@ -29,7 +29,10 @@ namespace EasyAI.Navigation.Generators
                     if(j > 0 && j < z - 1){//if within the boundaries of RangeZ
                         if(NodeArea.IsOpen(i, j) && !NodeArea.IsOpen(i, j-1)){//current area is open and last one was closed
                             if(isConvex(i, j-1)){//check if last area was a convex corner
-                            
+                                //so according to the research I've done, most node corner graph algorithms check the diagonal area around a node
+                                //at somepoint after discovering the convex corners. I've been reading the slides to try and understand exactly how this point of the algorithm works.
+                                //I've seen episode 2 of this series: https://www.youtube.com/watch?v=-L-WgKMFuhE&list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW , thinking it might help but 
+                                //the creator didn't seem to check diagonals. I'm unsure as to what sould happen if a diagonal is open versus closed. Would it be used as one of the corner's sides in Place()?
                                 Place(i, j - 1);//calculate where the node for this corner should be
                             }
                         }
@@ -64,6 +67,9 @@ namespace EasyAI.Navigation.Generators
             }
         }
 
+        //Additionally, Place() may be wrong, I've viewed the slides for one of the classes, and it seems to indicate that I should iterate in a forloop a square of positions cornerNodeSteps-long on each side.
+        //The slides colour the squares green so I assume they must be checked to see if they are open? I doesn't say what should happen if not all positions in the square are open. I've seen pseudo code to indicate
+        //that some sort of average must be calculated, but I'm not sure. 
         
         //Takes the position of a corner and calculates a new position to place the node for that corner
         public void Place(int corner_x, int corner_z){
