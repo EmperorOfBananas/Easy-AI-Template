f73edbbc0b3b7bd69e9d5fcfb05648e02c6501f6
EmperorOfBananas <acerenn01@gmail.com>
2023-04-21 21:30:44 -0400

    new nav files

--- Assets/Scripts/EasyAI/Navigation/AStar.cs
+++ Assets/Scripts/EasyAI/Navigation/AStar.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
-using EasyAI.Navigation.Nodes;
 using System.Linq;
-using System;
+using EasyAI.Navigation.Nodes;
 using UnityEngine;
 
 namespace EasyAI.Navigation
@@ -20,66 +19,81 @@ namespace EasyAI.Navigation
         /// <returns>The path of nodes to take to get from the starting position to the ending position.</returns>
         public static List<Vector3> Perform(Vector3 current, Vector3 goal, List<Connection> connections)
         {
-            // TODO - Assignment 4 - Implement A* pathfinding.
+            AStarNode best = null;
+        
+            // Add the starting position to the list of nodes.
+            List<AStarNode> aStarNodes = new() { new(current, goal) };
 
-            //I don't believe there is any problem with my current implementation of A*, the math seems to work, the only thing that breaks it is when goal nodes come with no connections,
-            //meaning no valid path can be found. But A* is supposed to be a complete algorithm so this cannot be the case. I feel the error must lie in the CornerGRaphGenerator.
+            // Loop until there are no options left meaning the path cannot be completed.
+            while (aStarNodes.Any(n => n.IsOpen))
+            {
+                // Get the open node with the lowest F cost and then by the lowest H cost if there is a tie in F cost.
+                AStarNode node = aStarNodes.Where(n => n.IsOpen).OrderBy(n => n.CostF).ThenBy(n => n.CostH).First();
             
-            AStarNode start = new AStarNode(current, goal);//initialize start of algorithm
-            List<AStarNode> open = new List<AStarNode>();//initialize open list of nodes
-            List<AStarNode> closed = new List<AStarNode>();//initialize closed list of nodes
-            open.Add(start);// add start to open
-            AStarNode curr = start;// make start current node
-            List<Vector3> path = new List<Vector3>();//initialize path to return for lookup calculation
-            /**
-            *forloop below checks if the goal node has neighbours and printes the result.
-            **/
-            bool test = false;//assume all goal nodes have connections
-            foreach (Connection conn in connections){
-                if(conn.A.Equals(goal)){//if a connection in connections has goal as one of it's members, it has at least 1 neighbour and is therefore traversable
-                    test = true;
+                // Close the current node.
+                node.Close();
+            
+                // Update this to the best node if it is.
+                if (best == null || node.CostF < best.CostF)
+                {
+                    best = node;
                 }
-            }
-            Debug.Log("goal: " + goal + " neighbours: " + test);//debug which nodes have connections and which don't
-            //A* algorithm loop
-            while(open.Count > 0){//loop continues until either goal is reached or we run out of open nodes
-                curr = visit(open);//visit the most appropriate node in the open list
-                open.Remove(curr);//remove it from open list
-                curr.Close();//close it
-                closed.Add(curr);//add to closed list
-                if(curr.Position == goal){//if goal has been found, break loop
-                    break;
-                }
-                //for each connection where one of the nodes is the current one and the other is not already in the open or closed lists. Add a new AStarNode
-                foreach (Connection conn in connections){
-                    if(conn.A.Equals(curr.Position) && !(closed.Any(c => c.Position == conn.B)) && !(open.Any(o => o.Position == conn.B))){
-                        open.Add(new AStarNode(conn.B, goal, curr));
-                    }
-                }
-            }
-            //get the path
-            while(curr.Position != start.Position){//if current position is not the start position
-                path.Add(curr.Position);//add previous positions to path so we can trace a path from the goal back to the start using the Previous AStarNode property
-                curr = curr.Previous;
-            }
-            if(curr.Position == start.Position){//If we have reached the start node, we have completed the path
-                path.Add(curr.Position);
-            }
-            return path;//return path to goal from start
-        }
+            
+                // Loop through all nodes which connect to the current node.
+                foreach (Connection connection in connections.Where(c => c.A == node.Position || c.B == node.Position))
+                {
+                    // Get the other position in the connection so we do not work with the exact same node again and get stuck.
+                    Vector3 position = connection.A == node.Position ? connection.B : connection.A;
+                
+                    // Create the A* node.
+                    AStarNode successor = new(position, goal, node);
 
-        //this function resurns a node with the lowest H cost, if there is a tie, it is decided
-        //by FIFO
-        public static AStarNode visit(List<AStarNode> open){
-            AStarNode select = open.ElementAt(0);//select first element in open
-            foreach (AStarNode o in open){//each node in open is judged if it has an equal or lower CostH than 
-                if(o.IsOpen){//if current node is not closed, has not been selected as curr in A* algo yet
-                    if(o.CostH <= select.CostH || !select.IsOpen){//if select is closed, cannot be returned as new value for curr or if o is <= to CostH of select, select is equal to o
-                        select = o;
+                    // If this node is the goal destination, A* is done so set it as the best and clear the node list so the loop ends.
+                    if (position == goal)
+                    {
+                        best = successor;
+                        aStarNodes.Clear();
+                        break;
                     }
+
+                    // If the node is not yet in the list, add it.
+                    AStarNode existing = aStarNodes.FirstOrDefault(n => n.Position == position);
+                    if (existing == null)
+                    {
+                        aStarNodes.Add(successor);
+                        continue;
+                    }
+
+                    // If it did already exist in the list but this path takes longer do nothing. 
+                    if (existing.CostF <= successor.CostF)
+                    {
+                        continue;
+                    }
+
+                    // If the new path is shorter, update its previous node and open it again.
+                    existing.UpdatePrevious(node);
+                    existing.Open();
                 }
             }
-            return select;
+
+            // If there was no best node which should never happen, simply return a line between the start and end positions.
+            if (best == null)
+            {
+                return new() { current, goal };
+            }
+
+            // Go from the last to node to the first adding all positions to the path.
+            List<Vector3> path = new();
+            while (best != null)
+            {
+                path.Add(best.Position);
+                best = best.Previous;
+            }
+
+            // Reverse the path so it is from start to goal and return it.
+            path.Reverse();
+        
+            return path;
         }
     }
 }
--- Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
+++ Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System;
 
 namespace EasyAI.Navigation.Generators
 {
@@ -12,84 +11,155 @@ namespace EasyAI.Navigation.Generators
         [Min(0)]
         [Tooltip("How far away from corners should the nodes be placed.")]
         private int cornerNodeSteps = 3;
-        public int x;//Length of NodeArea
-        public int z;//Width of NodeArea
     
         /// <summary>
         /// Place nodes at convex corners.
         /// </summary>
         public override void Generate()
         {
-            // TODO - Assignment 4 - Complete corner-graph node generation.
-
-            x = NodeArea.RangeX;
-            z = NodeArea.RangeZ;
-            for(int i = 0; i < x; i++){
-                for(int j = 0; j < z; j++){
-                    if(j > 0 && j < z - 1){//if within the boundaries of RangeZ
-                        if(NodeArea.IsOpen(i, j) && !NodeArea.IsOpen(i, j-1)){//current area is open and last one was closed
-                            if(isConvex(i, j-1)){//check if last area was a convex corner
-                                //so according to the research I've done, most node corner graph algorithms check the diagonal area around a node
-                                //at somepoint after discovering the convex corners. I've been reading the slides to try and understand exactly how this point of the algorithm works.
-                                //I've seen episode 2 of this series: https://www.youtube.com/watch?v=-L-WgKMFuhE&list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW , thinking it might help but 
-                                //the creator didn't seem to check diagonals. I'm unsure as to what should happen if a diagonal is open versus closed. Would it be used as one of the corner's sides in Place()?
-                                Place(i, j - 1);//calculate where the node for this corner should be
-                            }
-                        }
-                        if(!NodeArea.IsOpen(i, j) && NodeArea.IsOpen(i, j-1)){//current area is closed and last one was open
-                            if(isConvex(i, j)){//check if current area is a convex corner
-                                Place(i, j);//calculate where the node for this corner should be
-                            }
-                        }
+            // Check all X coordinates, skipping the padding required.
+            for (int x = cornerNodeSteps * 2; x < NodeArea.RangeX - cornerNodeSteps * 2; x++)
+            {
+                // Check all Z coordinates, skipping the padding required.
+                for (int z = cornerNodeSteps * 2; z < NodeArea.RangeZ - cornerNodeSteps * 2; z++)
+                {
+                    // If this space is open it cannot be a corner so continue.
+                    if (NodeArea.IsOpen(x, z))
+                    {
+                        continue;
                     }
+                
+                    // Otherwise it could be a corner so check in all directions.
+                    UpperUpper(x, z);
+                    UpperLower(x, z);
+                    LowerUpper(x, z);
+                    LowerLower(x, z);
                 }
             }
         }
 
-        //Takes the position of a potential convex corner and return true if it is so or false if otherwise
-        public bool isConvex(int cur_x, int cur_z){
-            int convex_check = 0;
-            //if there is a direction on the vertical axis that is open, 
-            //it will mean that this area has two open areas adjacent to each other.
-            //this would make it a convex corner
-            if(0 < cur_x){
-                if(NodeArea.IsOpen(cur_x - 1, cur_z)){convex_check++;}
-            }     
-            if(cur_x < x - 1){
-                if(NodeArea.IsOpen(cur_x + 1, cur_z)){convex_check++;}
+        /// <summary>
+        /// Check coordinates for an open corner to place a node in the positive X and positive Z directions.
+        /// </summary>
+        /// <param name="x">The X coordinate of the potential corner.</param>
+        /// <param name="z">The Z coordinate of the potential corner.</param>
+        private void UpperUpper(int x, int z)
+        {
+            // If the adjacent X or Z nodes are not open, return as it is not a convex corner.
+            if (!NodeArea.IsOpen(x + 1, z) || !NodeArea.IsOpen(x, z + 1))
+            {
+                return;
             }
-            
-            if(convex_check > 0){
-                return true;
+        
+            // Loop through all X coordinates to check the required space to place a node.
+            for (int x1 = x + 1; x1 <= x + 1 + cornerNodeSteps * 2; x1++)
+            {
+                // Loop through all Z coordinates to check the required space to place a node.
+                for (int z1 = z + 1; z1 <= z + 1 + cornerNodeSteps * 2; z1++)
+                {
+                    // If the node is not open return as there is no enough space to place the node.
+                    if (!NodeArea.IsOpen(x1, z1))
+                    {
+                        return;
+                    }
+                }
             }
-            else{
-                return false;
-            }
+
+            // Place the node at the given offset from the convex corner.
+            NodeArea.AddNode(x + 1 + cornerNodeSteps, z + 1 + cornerNodeSteps);
         }
 
-        //Additionally, Place() may be wrong, I've viewed the slides for one of the classes, and it seems to indicate that I should iterate in a forloop a square of positions cornerNodeSteps-long on each side.
-        //The slides colour the squares green so I assume they must be checked to see if they are open? I doesn't say what should happen if not all positions in the square are open. I've seen pseudo code to indicate
-        //that some sort of average must be calculated, but I'm not sure. 
+        /// <summary>
+        /// Check coordinates for an open corner to place a node in the positive X and negative Z directions.
+        /// </summary>
+        /// <param name="x">The X coordinate of the potential corner.</param>
+        /// <param name="z">The Z coordinate of the potential corner.</param>
+        private void UpperLower(int x, int z)
+        {
+            // If the adjacent X or Z nodes are not open, return as it is not a convex corner.
+            if (!NodeArea.IsOpen(x + 1, z) || !NodeArea.IsOpen(x, z - 1))
+            {
+                return;
+            }
+
+            // Loop through all X coordinates to check the required space to place a node.
+            for (int x1 = x + 1; x1 <= x + 1 + cornerNodeSteps * 2; x1++)
+            {
+                // Loop through all Z coordinates to check the required space to place a node.
+                for (int z1 = z - 1; z1 >= z - 1 - cornerNodeSteps * 2; z1--)
+                {
+                    // If the node is not open return as there is no enough space to place the node.
+                    if (!NodeArea.IsOpen(x1, z1))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            // Place the node at the given offset from the convex corner.
+            NodeArea.AddNode(x + 1 + cornerNodeSteps, z - 1 - cornerNodeSteps);
+        }
+    
+        /// <summary>
+        /// Check coordinates for an open corner to place a node in the negative X and positive Z directions.
+        /// </summary>
+        /// <param name="x">The X coordinate of the potential corner.</param>
+        /// <param name="z">The Z coordinate of the potential corner.</param>
+        private void LowerUpper(int x, int z)
+        {
+            // If the adjacent X or Z nodes are not open, return as it is not a convex corner.
+            if (!NodeArea.IsOpen(x - 1, z) || !NodeArea.IsOpen(x, z + 1))
+            {
+                return;
+            }
         
-        //Takes the position of a corner and calculates a new position to place the node for that corner
-        public void Place(int corner_x, int corner_z){
-            int node_x, node_z;
-            //if a direction leading away from the corner is clear, node_x will go 4 positions towards that direction
-            if(NodeArea.IsOpen(corner_x - 1, corner_z)){
-                node_x = corner_x - cornerNodeSteps;
+            // Loop through all X coordinates to check the required space to place a node.
+            for (int x1 = x - 1; x1 >= x - 1 - cornerNodeSteps * 2; x1--)
+            {
+                // Loop through all Z coordinates to check the required space to place a node.
+                for (int z1 = z + 1; z1 <= z + 1 + cornerNodeSteps * 2; z1++)
+                {
+                    // If the node is not open return as there is no enough space to place the node.
+                    if (!NodeArea.IsOpen(x1, z1))
+                    {
+                        return;
+                    }
+                }
             }
-            else{
-                node_x = corner_x + cornerNodeSteps;
+
+            // Place the node at the given offset from the convex corner.
+            NodeArea.AddNode(x - 1 - cornerNodeSteps, z + 1 + cornerNodeSteps);
+        }
+
+        /// <summary>
+        /// Check coordinates for an open corner to place a node in the negative X and negative Z directions.
+        /// </summary>
+        /// <param name="x">The X coordinate of the potential corner.</param>
+        /// <param name="z">The Z coordinate of the potential corner.</param>
+        private void LowerLower(int x, int z)
+        {
+            // If the adjacent X or Z nodes are not open, return as it is not a convex corner.
+            if (!NodeArea.IsOpen(x - 1, z) || !NodeArea.IsOpen(x, z - 1))
+            {
+                return;
             }
-            //same as above
-            if(NodeArea.IsOpen(corner_x, corner_z - 1)){
-                node_z = corner_z - cornerNodeSteps;
+        
+            // Loop through all X coordinates to check the required space to place a node.
+            for (int x1 = x - 1; x1 >= x - 1 - cornerNodeSteps * 2; x1--)
+            {
+                // Loop through all Z coordinates to check the required space to place a node.
+                for (int z1 = z - 1; z1 >= z - 1 - cornerNodeSteps * 2; z1--)
+                {
+                    // If the node is not open return as there is no enough space to place the node.
+                    if (!NodeArea.IsOpen(x1, z1))
+                    {
+                        return;
+                    }
+                }
             }
-            else{
-                node_z = corner_z + cornerNodeSteps;
-            }
-            //add node at position (node_x, node_z)
-            NodeArea.AddNode(node_x, node_z);
+
+            // Place the node at the given offset from the convex corner.
+            NodeArea.AddNode(x - 1 - cornerNodeSteps, z - 1 - cornerNodeSteps);
         }
     }
 }
